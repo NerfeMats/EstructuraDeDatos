2dcb2357f415250984df771ed33bcd4b74f5f694
NerfeMats <yermo60@hotmail.com>
2021-01-25 14:17:29 -0600

    de agrega la funcion insertar producto, estudiar codigo

--- tareaMejor200121/main.cpp
+++ tareaMejor200121/main.cpp
@@ -1,36 +1,6 @@
 #include <iostream>
 
 using namespace std;
-class cNodo{
-public:
-
-string producto;
-int precio;
-int cantidad;
-
-cNodo(string prod, int prec, int can )
-{
-producto=prod;
-precio = prec;
-cantidad = can;
-}
-
-cNodo* sig;
-
-void imprimir()
-{
-cout<<"producto: "<<producto<<endl;
-cout<<"precio: "<<precio<<endl;
-cout<<"cantidad: "<<cantidad<<endl;
-
-}
-//la cantidad del producto en unidades por ejemplo
-//5 unidades de este producto. El minimo valor es 1
-//el maximo es 20
-
-};
-
-
 string regresaNombre(int i){
 return "Producto"+to_string(i);
 }
@@ -43,23 +13,82 @@ int precioAleatorio(){
 return rand() % 20 +1;
 }
 
+
+
+class cNodo{
+public:
+
+string producto;
+int precio;
+int cantidad;
+cNodo* sig;
+
+cNodo(string prod, int prec, int can )
+{
+producto=prod;
+precio = prec;
+cantidad = can;
+sig=NULL;
+}
+
+
+
+void imprimir()
+{
+cout<<"producto: "<<producto<<endl;
+cout<<"precio: "<<precio<<endl;
+cout<<"cantidad: "<<cantidad<<endl;
+
+}
+//la cantidad del producto en unidades por ejemplo
+//5 unidades de este producto. El minimo valor es 1
+//el maximo es 20
+
+
+
+
+
+
+};
+
 class cListaLigada{
     public:
+
 cNodo* inicio;
+cNodo* aux;
 int n;
-void crearListaLigada()
-{
 
-}
+ cListaLigada()
+ {
+   inicio=NULL;
+ }
 
-void imprimir(){
+    void insertarProducto(string prod, int pre, int can)
+    {
+        if (inicio==NULL) // no hay productos
+            {inicio = new cNodo(prod,pre,can);
+            aux=inicio;}
+        else
+        {
 
-cNodo* aux=inicio;
-for(int i=0; i<n;i++){
-aux->imprimir();
-aux=aux->sig;
-}
+            aux->sig = new cNodo(prod,pre,can);
+            aux=aux->sig;
 
+        }
+    }
+
+
+    void imprimir(){
+
+    if(inicio!=NULL)
+    {
+        cNodo* aux=inicio;
+        while(aux!=NULL){
+        aux->imprimir();
+        aux=aux->sig;
+        }
+    } else
+      cout<<"La lista esta vacia"<<endl;
 
 }
 
@@ -67,9 +96,13 @@ aux=aux->sig;
 
 
 
+
 int main()
 {
     cListaLigada listaparaCarShop1;
+    listaparaCarShop1.insertarProducto("prodA", 34, 2);
+    listaparaCarShop1.insertarProducto("prodB", 4, 5);
+    listaparaCarShop1.insertarProducto("prodC", 40, 2);
     listaparaCarShop1.imprimir();
 
     cListaLigada listaparaCarShop2;
