ef09a6acc1369ca19e8a9469964e5b8906a76acc
NerfeMats <yermo60@hotmail.com>
2021-02-10 11:33:41 -0600

    clase 10-02-2021

--- despuesDelExamen/main.cpp
+++ despuesDelExamen/main.cpp
@@ -9,13 +9,14 @@ cNodo * sig;
 
 cNodo(int p)
 {
-this->i=p;
+this->i = p;
 this->sig=NULL;
 }
 
 void imprimir()
 {
-    cout<<i<<endl;
+
+    cout<<"información: "<<i<<endl;
 }
 };
 
@@ -52,7 +53,6 @@ else{
       mifinal->sig  = nuevofinal;
       mifinal = mifinal->sig;
 }
-
 }
 
 void agregarInfSiempreAlInicio(int i){
@@ -88,6 +88,44 @@ cout<<"la lista esta vacia"<<endl;
 
 }
 
+int quitarPrimerElemento()
+{
+ int p = inicio->i;
+ cNodo* aux = inicio;
+ inicio=inicio->sig;
+ aux->sig = NULL;
+ delete aux;
+ return p;
+}
+
+void quitarElFinal()
+{
+
+cNodo* aux=inicio;
+if(!this->estaVacia())
+{
+    if(aux->sig!=NULL)
+    {
+        while(aux->sig->sig!=NULL)
+        aux=aux->sig;
+        aux->sig = NULL;
+        delete this->mifinal;
+        this->mifinal = aux;
+    }
+    else {
+
+        //cout<<"hay un solo elemento"<<endl;
+        inicio=NULL;
+    }
+}
+else
+    cout<<"Lista vacia"<<endl;
+
+
+
+}
+
+
 
 bool buscar(int i)
 {
@@ -117,29 +155,33 @@ else
 
 };
 
-
-
-
 int main()
 {
     cListaLigada* l1 = new cListaLigada;
     for(int i=0; i<20;i++)
     l1->agregarInf(i);
     l1->imprimir();
+    cout<<"se elimina el elemento: "<<l1->quitarPrimerElemento()<<endl;
+    l1->imprimir();
     cout<<"agregar otro\n";
     l1->agregarInf(20);
+    cout<<"Eliminar el final"<<endl;
+    l1->quitarElFinal();
+    l1->quitarElFinal();
+    l1->quitarElFinal();
     l1->imprimir();
 
     cout<<"______________________"<<endl;
 
+    system("pause");
     cListaLigada* l2 = new cListaLigada;
-    for(int i=0; i<20;i++)
-    l2->agregarInfSiempreAlInicio(i);
+    l2->agregarInf(0);
+    l2->agregarInf(1);
     l2->imprimir();
-    cout<<"agregar otro\n";
-    l2->agregarInfSiempreAlInicio(20);
+    l2->quitarElFinal();
     l2->imprimir();
 
+    system("pause");
 
 cout<<"_________________"<<endl;
     cListaLigada* l3 = new cListaLigada;
